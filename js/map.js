ec98a63c3a28f444dde899e3d925e3b93451e74e
vanminh1310 <taminh39@gmail.com>
2021-04-04 15:56:05 +0700

    commit

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@ firebase.initializeApp(firebaseConfig);
 var ref = firebase.database().ref("GPS");
 ref.on("value", gotData);
 var kinhdo;
-var vido; 
+var vido;
 String.prototype.format = function () {
     var content = this;
     for (var i = 0; i < arguments.length; i++) {
@@ -21,41 +21,59 @@ String.prototype.format = function () {
     }
     return content;
 };
+
 function gotData(data) {
     var scores = data.val();
     // Grab the keys to iterate over the object
     var keys = Object.keys(scores);
     // var textnode = document.createTextNode("Water"
-    
+
     for (var i = 0; i < keys.length; i++) {
-      var k = keys[i];
-      // Look at each fruit object!
-      vido = scores[k].Latitude;
-      kinhdo = scores[k].Longitude;
-      console.log("LA",vido,"LO",kinhdo)
-     
+        var k = keys[i];
+        // Look at each fruit object!
+        vido = scores[k].Latitude;
+        kinhdo = scores[k].Longitude;
+        console.log("LA", vido, "LO", kinhdo)
+
     }
-    console.log("LA",vido,"LO",kinhdo)
-    
+    console.log("LA", vido, "LO", kinhdo)
+
 }
-  
-function click_pass(){
-    var myModal= document.getElementById('modal')
-    var modal = bootstrap.Modal.getInstance(myModal)
+
+function click_pass() {
+    var myModal = document.getElementById('modal')
+    var btncheck = document.getElementById("btn-check-2");
+    var btncheck2 = document.getElementById("btn-check-21");
+
     var pass = document.getElementById('pass').value
     var passW = firebase.database().ref().child("pass")
-        passW.on('value',function(passW){
-       var pass2 = passW.val();
-       if(pass== pass2){
-       
-        $('#modal').modal('hide');
-        $('body').removeClass('modal-open');
-        $('.modal-backdrop').remove();  
-        console.log("test1");
-        
-    }
-   
-        });
+    passW.on('value', function (passW) {
+        var pass2 = passW.val();
+        if (pass == pass2 && btncheck.checked == true) {
+
+            $('#modal').modal('hide');
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+            console.log("test1");
+            // alert("Chế độ Checking Real Time")
+            document.getElementById('tab').style.display='none'
+            btncheck.style.backgroundColor = "red";
+        }
+        if (pass == pass2 && btncheck2.checked == true) {
+
+            $('#modal').modal('hide');
+          
+            $('.modal-backdrop').remove();
+            console.log("test12");
+            document.getElementById('tab').style.display='block'
+        }
+        else{
+            document.getElementById("tb").innerHTML="Mật khẩu sai rồi hay thử lại nhé!"
+        }
+
+
+
+    });
 
 }
 
@@ -64,7 +82,7 @@ function click_pass(){
 
 
 
-  
+
 
 
 
@@ -90,5 +108,4 @@ L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
 
 L.marker([10.8447125, 106.797841667]).addTo(map)
     .bindPopup('Tôi ở đây <br>Văn Minh')
-    //.bindPopup(vido)
-    
+//.bindPopup(vido)
