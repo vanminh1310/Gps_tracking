3344e958e1dff5e0773a2db41f7983f81685aa5e
vanminh1310 <taminh39@gmail.com>
2021-04-07 22:15:57 +0700

    commit

--- js/ecall.js
+++ js/ecall.js
@@ -1,3 +1,67 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyALyfBlOsvmKDwNQLTKurC5hf_BmFKYnr4",
+    authDomain: "vann-53570.firebaseapp.com",
+    databaseURL: "https://vann-53570.firebaseio.com",
+    projectId: "vann-53570",
+    storageBucket: "vann-53570.appspot.com",
+    messagingSenderId: "476713717043",
+    appId: "1:476713717043:web:93e1c456a05aa8b8130246",
+    measurementId: "G-8PRCPKPS2K"
+};
+firebase.initializeApp(firebaseConfig);
+var ref = firebase.database().ref("GPS");
+ref.on("value", gotData);
+var kinhdo;
+var vido;
+var btncheck = document.getElementById("btn-check-2");
+var btncheck2 = document.getElementById("btn-check-21");
+
+
+
+String.prototype.format = function () {
+    var content = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
+        content = content.replace(reg, arguments[i]);
+    }
+    return content;
+    
+};
+
+function gotData(data) {
+    var scores = data.val();
+    // Grab the keys to iterate over the object
+    var keys = Object.keys(scores);
+    // var textnode = document.createTextNode("Water"
+
+    for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        // Look at each fruit object!
+        vido = scores[k].Latitude;
+        kinhdo = scores[k].Longitude;
+    
+         var row = $("<ul>"); row.css("cursor", "pointer");
+
+    row.append("<li class='col0'>{0},</li>".format(vido));
+    // row.append("<li class='col3'>{0}</li>".format(vido));
+    row.append("<li class='col3'>{0}</li>".format(kinhdo));
+    // row.append("<li class='col3'>{0},</li>".format(kinhdo));
+    row.click(function () { 
+      // alert($(this).text()); // get data from row on list 
+       var array = $(this).text().split(',',1)[0];
+       var array2 = $(this).text().split(',',2)[1];
+       L.marker([array, array2], {icon: greenIcon}).addTo(map)
+       map.setView([array,array2], 20); // zoom 
+       console.log(array)
+       console.log(array2)
+     }); // ham click
+    // sai jquery 
+    $( ".table-ul-body .table-ul" ).append(row);
+    }
+    console.log("LA",this.vido, "LO", this.kinhdo)
+
+}
+
 // tạo map
 var map = L.map('mapId').setView([10.8447125, 106.797841667], 5);
 //L.marker([10.8447125, 106.797841667], {icon: greenIcon}).addTo(map) .bindPopup();
